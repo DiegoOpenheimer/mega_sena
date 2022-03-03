264a7e42593f662fd745bd05ee1bf213f4b0c644
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2022-03-03 14:16:42 -0300

    Initial commit

---
+++ lib/home/DataHelper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DataHelper {
+  TextEditingController controller;
+  Key key;
+  bool typedValue;
+
+  DataHelper(
+      {required this.controller, required this.typedValue, required this.key});
+}
